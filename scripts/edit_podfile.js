29c9fadd5d45841bdd1a1f80d5ca5b8f8b6b32a7
SuraiyaShiratti <73700033+SuraiyaShiratti@users.noreply.github.com>
2024-03-13 20:45:27 +0530

    Update edit_podfile.js

--- scripts/edit_podfile.js
+++ scripts/edit_podfile.js
@@ -8,6 +8,7 @@ var configurations = require(`./configuration`);
     // Define the Podfile path
 const PODFILE_PATH = 'platforms/ios/Podfile';
 var PLATFORM = configurations.PLATFORM;
+const NEW_PLATFORM_LINE = `platform :ios, '17.0'\n`;
 
 function editPodfile() {
     const CONTENT = `
@@ -27,6 +28,15 @@ end
         console.error(`Error reading Podfile: ${err}`);
         return;
     }
+// Replace the platform line
+	const updatedContent = data.replace(/platform :ios, '\d+\.\d+'/g, NEW_PLATFORM_LINE);
+	fs.writeFile(PODFILE_PATH, updatedContent, 'utf8', (err) => {
+		if (err) {
+			console.error(`Error writing to Podfile: ${err}`);
+			return;
+		}
+		console.log(`Updated platform version in Podfile to 17.0`);
+	});
 
     // Check if the post_install section already exists
     if (!data.includes('post_install')) {
